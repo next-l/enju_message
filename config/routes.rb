09c878515df65a95e74812b69ce5b03c0968df57
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2011-10-10 22:03:59 +0900

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,9 @@
 Rails.application.routes.draw do
+  resources :message_requests, :except => [:new, :create]
+  resources :message_templates
+  resources :messages do
+    collection do
+      post :destroy_selected
+    end
+  end
 end
