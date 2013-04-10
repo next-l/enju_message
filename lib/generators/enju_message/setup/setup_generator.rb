cfc43b42507265202d8f5ce6a0e6c04899381a2b
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-04-11 04:38:51 +0900

    updated generator

--- lib/generators/enju_message/setup/setup_generator.rb
+++ lib/generators/enju_message/setup/setup_generator.rb
@@ -4,5 +4,7 @@ class EnjuMessage::SetupGenerator < Rails::Generators::Base
   def copy_setup_files
     directory("db/fixtures", "db/fixtures/enju_message")
     rake("enju_message_engine:install:migrations")
+    inject_into_file 'app/models/user.rb',
+      "  enju_message_user_model\n", :after => "enju_leaf_user_model\n"
   end
 end
