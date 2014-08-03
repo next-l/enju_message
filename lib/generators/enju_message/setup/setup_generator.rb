75e1ff910c329811b1acc648a8d1cb19d03cc4e4
Kosuke Tanabe <nabeta@fastmail.fm>
2014-08-04 00:31:48 +0900

    added templates
    Conflicts:
    	lib/generators/enju_message/setup/templates/db/fixtures/message_templates.yml

--- lib/generators/enju_message/setup/setup_generator.rb
+++ lib/generators/enju_message/setup/setup_generator.rb
@@ -1,8 +1,10 @@
 class EnjuMessage::SetupGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
+  argument :file, :type => :string, :default => "all"
 
   def copy_setup_files
     directory("db/fixtures", "db/fixtures/enju_message")
+    return if file == 'fixture'
     rake("enju_message_engine:install:migrations")
     inject_into_file 'app/models/user.rb',
       "  enju_message_user_model\n", :after => "enju_leaf_user_model\n"
