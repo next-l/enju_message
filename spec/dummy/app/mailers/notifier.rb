fdb29153f4384cb4ba87589a76dc8783186fd9b6
Kosuke Tanabe <nabeta@fastmail.fm>
2014-08-13 14:48:01 +0900

    updated dummy file

--- spec/dummy/app/mailers/notifier.rb
+++ spec/dummy/app/mailers/notifier.rb
@@ -11,20 +11,20 @@ class Notifier < ActionMailer::Base
 
   def message_notification(message_id)
     message = Message.find(message_id)
-    I18n.locale = message.receiver.locale.try(:to_sym) || I18n.default_locale
-    from = "#{LibraryGroup.system_name(message.receiver.locale)} <#{LibraryGroup.site_config.email}>"
+    I18n.locale = message.receiver.profile.locale.try(:to_sym) || I18n.default_locale
+    from = "#{LibraryGroup.system_name(message.receiver.profile.locale)} <#{LibraryGroup.site_config.email}>"
     if message.subject
       subject = message.subject
     else
-      subject = I18n.t('message.new_message_from_library', :library => LibraryGroup.system_name(message.receiver.user.locale))
+      subject = I18n.t('message.new_message_from_library', :library => LibraryGroup.system_name(message.receiver.user.profile.locale))
     end
     if message.sender
-      @sender_name = message.sender.full_name
+      @sender_name = message.sender.username
     else
-      @sender_name = LibraryGroup.system_name(message.receiver.locale)
+      @sender_name = LibraryGroup.system_name(message.receiver.profile.locale)
     end
     @message = message
-    @locale = message.receiver.locale
+    @locale = message.receiver.profile.locale
     mail(:from => from, :to => message.receiver.email, :subject => subject)
   end
 end
