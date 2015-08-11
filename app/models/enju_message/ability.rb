cf4c00a82a0117ef7d47e2b675948d132334bd70
Kosuke Tanabe <nabeta@fastmail.fm>
2015-08-11 13:40:46 +0900

    fix deleting selected messages next-l/enju_leaf#654

--- app/models/enju_message/ability.rb
+++ app/models/enju_message/ability.rb
@@ -6,6 +6,7 @@ module EnjuMessage
       case user.try(:role).try(:name)
       when 'Administrator'
         can :manage, Message
+        can :destroy_selected, Message
         can [:read, :update, :destroy, :delete], MessageRequest
         can [:read, :update], MessageTemplate
       when 'Librarian'
@@ -13,13 +14,13 @@ module EnjuMessage
         can [:update], Message do |message|
           message.sender == user
         end
-        can [:show, :destroy, :delete], Message do |message|
+        can [:show, :destroy, :delete, :destroy_selected], Message do |message|
           message.receiver == user
         end
         can [:read, :update, :destroy, :delete], MessageRequest
         can :read, MessageTemplate
       when 'User'
-        can [:read, :destroy, :delete], Message do |message|
+        can [:show, :destroy, :delete, :destroy_selected], Message do |message|
           message.receiver == user
         end
         can :index, Message
