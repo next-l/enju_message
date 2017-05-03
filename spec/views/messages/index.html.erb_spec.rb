faf52b3245c82a85ab65e7fa3e48ee4767a78622
Kosuke Tanabe <nabeta@fastmail.fm>
2017-05-04 04:55:27 +0900

    fix spec file

--- spec/views/messages/index.html.erb_spec.rb
+++ spec/views/messages/index.html.erb_spec.rb
@@ -3,21 +3,23 @@ require 'rails_helper'
 describe 'messages/index' do
   before(:each) do
     assign(:messages, Kaminari.paginate_array([
-                                                stub_model(Message,
-                                                           sender_id: 1,
-                                                           receiver_id: 2,
-                                                           created_at: Time.zone.now),
-                                                stub_model(Message,
-                                                           sender_id: 1,
-                                                           receiver_id: 2,
-                                                           created_at: Time.zone.now)
-                                              ]).page(1))
+      stub_model(Message,
+        sender_id: 1,
+        receiver_id: 2,
+        created_at: Time.zone.now),
+      stub_model(Message,
+        sender_id: 1,
+        receiver_id: 2,
+        created_at: Time.zone.now)
+    ]).page(1))
+    view.stub(:filtered_params).and_return(ActionController::Parameters.new(per_page: 50).permit(:per_page))
+    @message_facet = {}
   end
 
   it 'renders a list of messages' do
     allow(view).to receive(:policy).and_return double(create?: true, destroy?: true)
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select 'tr>td', count: 10
+    assert_select 'tr>td', count: 9
   end
 end
