66b194db2ede3309174e1a07b05c28a4a33bd20f
Kosuke Tanabe <nabeta@fastmail.fm>
2018-06-17 11:43:24 +0900

    update dummy migration files

--- spec/dummy/db/migrate/20140817155043_add_extent_of_text_to_manifestation.rb
+++ spec/dummy/db/migrate/20140817155043_add_extent_of_text_to_manifestation.rb
@@ -1,4 +1,4 @@
-class AddExtentOfTextToManifestation < ActiveRecord::Migration
+class AddExtentOfTextToManifestation < ActiveRecord::Migration[4.2]
   def change
     add_column :manifestations, :extent_of_text, :text
   end
