f280a97848e27d0c41c0312d200dde391a290768
Kosuke Tanabe <nabeta@fastmail.fm>
2018-10-29 19:00:49 +0900

    update factory files

--- spec/factories/profile.rb
+++ spec/factories/profile.rb
@@ -3,8 +3,8 @@ FactoryBot.define do
     f.user_group_id {UserGroup.first.id}
     f.required_role_id {Role.where(name: 'User').first.id}
     f.sequence(:user_number){|n| "user_number_#{n}"}
-    f.library_id 2
-    f.locale "ja"
+    f.library_id { 2 }
+    f.locale { 'ja' }
     after(:create) do |profile|
       user = FactoryBot.create(:user)
       profile.user = user
