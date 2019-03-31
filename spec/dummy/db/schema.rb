454cc76e9e2034b10b88a50534150752baf2841d
Kosuke Tanabe <nabeta@fastmail.fm>
2019-03-31 20:56:21 +0900

    update factory files

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180107162711) do
+ActiveRecord::Schema.define(version: 2018_01_07_162711) do
 
   create_table "accepts", force: :cascade do |t|
     t.integer "basket_id"
