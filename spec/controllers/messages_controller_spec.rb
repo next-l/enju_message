aac8f86a81e47b1fc1100f949e6107e1c7861e9a
Kosuke Tanabe <nabeta@fastmail.fm>
2019-03-31 21:26:44 +0900

    update spec file

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -264,6 +264,7 @@ describe MessagesController do
           response.should be_successful
         end
       end
+
       describe 'with invalid recipient' do
         it "re-renders the 'new' template" do
           post :create, params: { message: @invalid_user_attrs }
