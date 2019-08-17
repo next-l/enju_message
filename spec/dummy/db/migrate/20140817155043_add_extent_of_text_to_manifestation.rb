34d4c2e98a0c18fbc33db38172954d9aebb4a59f
Kosuke Tanabe <nabeta@fastmail.fm>
2019-08-17 19:24:07 +0900

    update migration version to 5.2

--- spec/dummy/db/migrate/20140817155043_add_extent_of_text_to_manifestation.rb
+++
@@ -1,5 +0,0 @@
-class AddExtentOfTextToManifestation < ActiveRecord::Migration[4.2]
-  def change
-    add_column :manifestations, :extent_of_text, :text
-  end
-end
--- spec/dummy/db/migrate/20180107161331_add_constraints_to_most_recent_for_resource_import_file_transitions.rb
+++ spec/dummy/db/migrate/20180107161331_add_constraints_to_most_recent_for_resource_import_file_transitions.rb
@@ -1,8 +1,8 @@
-class AddConstraintsToMostRecentForResourceImportFileTransitions < ActiveRecord::Migration[4.2]
+class AddConstraintsToMostRecentForResourceImportFileTransitions < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
 
   def up
-    add_index :resource_import_file_transitions, [:resource_import_file_id, :most_recent], unique: true, where: "most_recent", name: "index_resource_import_file_transitions_parent_most_recent" # , algorithm: :concurrently
+    add_index :resource_import_file_transitions, [:resource_import_file_id, :most_recent], unique: true, where: "most_recent", name: "index_resource_import_file_transitions_parent_most_recent" #, algorithm: :concurrently
     change_column_null :resource_import_file_transitions, :most_recent, false
   end
 
