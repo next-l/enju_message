f88b560ed67ca4b6f38585d87fdb41b305d9ce0d
Kosuke Tanabe <nabeta@fastmail.fm>
2020-04-24 23:57:45 +0900

    add webpack config files

--- spec/dummy/app/javascript/packs/application.js
+++ spec/dummy/app/javascript/packs/application.js
@@ -1,6 +1,9 @@
 import 'bootstrap'
+<<<<<<< HEAD
 import '../src/cocoon.js'
 import '../src/application.scss'
+=======
+>>>>>>> a692d71... add webpack config files
 
 import Rails from '@rails/ujs'
 Rails.start()
